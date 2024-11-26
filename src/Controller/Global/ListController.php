4cc69f4d84cfd25aa2e8478daecc1b6142ba47d6
FaziGIT <tsrt.mathis@gmail.com>
2024-11-26 12:59:58 +0100

    feat(lists): inject lists in <select>

--- src/Controller/Global/ListController.php
+++ src/Controller/Global/ListController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Global;
 
 use App\Repository\MediaRepository;
+use App\Repository\PlaylistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,12 +11,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class ListController extends AbstractController
 {
     #[Route('/lists', name: 'app_lists')]
-    public function index(MediaRepository $mediaRepository): Response
+    public function index(MediaRepository $mediaRepository, PlaylistRepository $playlistRepository): Response
     {
         $medias = $mediaRepository->findAll();
+        $allPlaylists = $playlistRepository->findAll();
 
         return $this->render('other/lists.html.twig', [
             'medias' => $medias,
+            'allPlaylists' => $allPlaylists,
         ]);
     }
 }
