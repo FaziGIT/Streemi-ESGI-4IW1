9b6dd63050cfefc3481e22dc7bdeb4d48de65f2e
FaziGIT <mathis.tsrt@gmail.com>
2024-12-17 00:17:45 +0100

    feat(authentification):  add a is_granted to acces to admin pannel

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
         return $this->render('admin/admin.html.twig', [
@@ -17,6 +19,7 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/users', name: 'app_admin_users')]
+    #[IsGranted('ROLE_ADMIN')]
     public function users(): Response
     {
         return $this->render('admin/admin_users.html.twig', [
@@ -25,6 +28,7 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/films', name: 'app_admin_movies')]
+    #[IsGranted('ROLE_ADMIN')]
     public function movies(): Response
     {
         return $this->render('admin/admin_films.html.twig', [
@@ -33,6 +37,7 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/films/add', name: 'app_admin_films_add')]
+    #[IsGranted('ROLE_ADMIN')]
     public function addMovie(): Response
     {
         return $this->render('admin/admin_add_films.html.twig', [
