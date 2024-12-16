6677611269884671b502d8c51f600bdd30b6b6ed
FaziGIT <mathis.tsrt@gmail.com>
2024-12-17 00:17:40 +0100

    feat(authentification): add a is_granted to acces to admin pannel

--- src/Controller/Global/ListController.php
+++ src/Controller/Global/ListController.php
@@ -7,10 +7,12 @@ use App\Repository\PlaylistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ListController extends AbstractController
 {
     #[Route('/lists', name: 'app_lists')]
+    #[IsGranted('ROLE_USER')]
     public function index(MediaRepository $mediaRepository, PlaylistRepository $playlistRepository): Response
     {
         $medias = $mediaRepository->findAll();
--- src/Controller/Global/SubscriptionController.php
+++ src/Controller/Global/SubscriptionController.php
@@ -6,10 +6,12 @@ use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SubscriptionController extends AbstractController
 {
     #[Route('/subscription', name: 'app_subscription')]
+    #[IsGranted('ROLE_USER')]
     public function index(SubscriptionRepository $subscriptionRepository): Response
     {
         $subscriptions = $subscriptionRepository->findAll();
